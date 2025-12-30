c24124bfb90f80941f54fbd3d1a8e114a95fa32d
Kunal <kunaljsr11@gmail.com>
2025-12-30 20:48:06 +0530

    fix: Update OutputSection to use dynamic import for Editor and adjust type reference

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -3,13 +3,14 @@ import React, { useEffect, useRef } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 import { useTheme } from 'next-themes'
-import { Editor } from '@toast-ui/react-editor';
+import dynamic from 'next/dynamic';
+const Editor = dynamic(() => import('@toast-ui/react-editor').then(mod => mod.Editor), { ssr: false });
 import { Button } from '@/components/ui/button';
 import { Copy } from 'lucide-react';
 
 const OutputSection = ({ aiResponse }: { aiResponse: string }) => {
   const { theme } = useTheme();
-  const editorRef = useRef<Editor>(null);
+  const editorRef = useRef<any>(null);
 
   useEffect(() => {
     const editorInstance = editorRef.current?.getInstance();
@@ -18,16 +19,16 @@ const OutputSection = ({ aiResponse }: { aiResponse: string }) => {
     }
   }, [aiResponse])
 
- 
+
 
   return (
-    <div spellCheck={false} className='bg-white/40 dark:bg-white/5 dark:shadow-cyan-900 shadow-lg shadow-cyan-100 rounded-lg [&_.toastui-editor-defaultUI]:!bg-inherit [&_.toastui-editor]:!bg-inherit [&_.toastui-editor-main]:!bg-inherit [&_.toastui-editor-ww-container]:!bg-inherit [&_.toastui-editor-toolbar]:!bg-cyan-950'>
+    <div spellCheck={false} className='bg-white/40 dark:bg-white/5 dark:shadow-cyan-900 shadow-lg shadow-cyan-100 rounded-lg [&_.toastui-editor-defaultUI]:!bg-inherit [&_.toastui-editor]:!bg-inherit [&_.toastui-editor-main]:!bg-inherit [&_.toastui-editor-ww-container]:!bg-inherit [&_.toastui-editor-toolbar]:!bg-cyan-950 [&_.toastui-editor-contents]:!text-black  '>
       <div className='flex justify-between items-center p-5'>
         <h2 className='font-medium text-lg'>Your Result</h2>
         <Button
-         className='flex bg-black/70 dark:bg-cyan-800 dark:text-white hover:bg-black/70 cursor-pointer gap-2 text-sm'
-         onClick={()=> navigator.clipboard.writeText(editorRef.current?.getInstance().getMarkdown() || '')}
-         > <Copy className='w-3 h-3' />Copy </Button>
+          className='flex bg-black/70 dark:bg-cyan-800 dark:text-white hover:bg-black/70 cursor-pointer gap-2 text-sm'
+          onClick={() => navigator.clipboard.writeText(editorRef.current?.getInstance().getMarkdown() || '')}
+        > <Copy className='w-3 h-3' />Copy </Button>
       </div>
       <Editor
         ref={editorRef}
